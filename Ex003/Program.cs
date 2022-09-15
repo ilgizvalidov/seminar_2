1a18bde3af4dfe7127d0d945e78c67f5c2493824
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-15 11:32:05 +0300

    программа прнимает на вход два числа, если второе число кратно первому выводит сообщение что число кратное, иначе выводит отстаток от деления

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿void Kraton (int arg1, int arg2)
+{
+    if (arg1 % arg2 == 0) Console.WriteLine ("второе число кратно первому");
+    else Console.WriteLine("второе число не кратно первому, остаток от деления равен " + arg1%arg2);
+}
+
+try
+{
+    Console.WriteLine("введите первое целое число число");
+    int Number1 = Convert.ToInt32(Console.ReadLine ());
+    Console.WriteLine("Введите второе целое число");
+    int Number2 = Convert.ToInt32 (Console.ReadLine());
+    
+    Kraton(Number1, Number2);
+}
+catch
+{
+    Console.WriteLine("вы ввели не целое число");
+}
+
