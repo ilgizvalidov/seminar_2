5f4a56c2e5ee9813208b2b125c2cc826dba8ac68
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-15 16:51:20 +0300

    программа принимает на вход два целых цисла  и выдает сообщение явлется ли одно число квадратом другого

--- Ex005_zadacha_16/Program.cs
+++ Ex005_zadacha_16/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Kvadrat (int arg1, int arg2)
+{
+    if (arg1*arg1 == arg2) Console.WriteLine ($" Число {arg2} является квадратом числа {arg1}");    
+   
+    else if (arg2*arg2 == arg1) Console.WriteLine ($"Число {arg1} является квадратом числа {arg2}"); 
+         else Console.WriteLine ("ни одно число не является квадратом другого");
+}
+
+try {
+    Console.WriteLine ("Введите первое целое число");
+    int Number1 = Convert.ToInt32 (Console.ReadLine());
+    Console.WriteLine ("Введите второе целое число");
+    int Number2 = Convert.ToInt32 (Console.ReadLine ()); 
+    Kvadrat (Number1, Number2);
+   
+}    
+
+catch {
+    Console.WriteLine ("вы ввели не целое число");
+}
