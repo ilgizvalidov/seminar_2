479ca974fbd0b3ad1ac3d8961fe928902c0637d2
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-15 16:21:03 +0300

    программа принимает на вход целое число и проверяет его на кратность числам 7 и 23, если число кратно выдает ответ да, иначе нет

---
+++ Ex004_zadacha_14/Program.cs
@@ -0,0 +1,20 @@
+﻿// задача 14. Написать программу, которая принимает на вход число и проверяет его на кратность одновременно 7 и 23
+
+
+void Kraton (int arg1)
+{
+    if (arg1 % 7 == arg1 % 23) Console.WriteLine ("да"); // если число делится на 7 и 23 без остатка (остаток равен 0) то чило кратное паре 7 и 23
+    else Console.WriteLine("нет"); 
+    
+}
+
+try
+{
+    Console.WriteLine ("введите целое число");
+    int Number = Convert.ToInt32 (Console.ReadLine());
+    Kraton (Number);
+}
+catch 
+{
+    Console.WriteLine ("введите целое число");
+}
