d6a0e82f6b0ad34d84a2e011bc3009c80e4265a7
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-15 10:34:53 +0300

    программа выводит случайное числои из отрезка 10 - 99 и выдает какая цифра числа больше и выводит ее

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+   int  Number = new Random().Next(10,100); // используем () для отрезока от 10 до 99, так как правая граница не берется.
+   int Number1 = Number/10;
+   int Number2 = Number%10;
+   Console.WriteLine($"Сгенерировано число {Number}"); 
+   if (Number1 > Number2) 
+    {
+        Console.WriteLine($"первая цифра больше и равна {Number1}");
+    }  
+    else
+    {
+        if (Number2 > Number1 ) 
+        {
+            Console.WriteLine($"вторая цифра больше и равна {Number2}");
+        }
+        else 
+        {
+            Console.WriteLine("Это полиндром");    
+        }
+            
+    }
